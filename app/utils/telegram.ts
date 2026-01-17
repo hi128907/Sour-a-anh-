419500d915bf0b39bf3bb44e6e4532ebc57edd1a
hi128907 <alis.onlaforge362@gmail.com>
2026-01-18 05:50:23 +0700

    Update telegram.ts

--- app/utils/telegram.ts
+++ app/utils/telegram.ts
@@ -3,8 +3,8 @@ import https from 'https';
 
 const agent = new https.Agent({ family: 4 });
 
-const TELEGRAM_API = `https://still-hat-1e87.ductham784.workers.dev/bot${process.env.8581345348:AAH0HJxhs1hJonuQ3ZiQBYo5JLsCRVlKeiU}`;
-const CHAT_ID = process.env.-5011906253!; 
+const TELEGRAM_API = `https://api.telegram.org/bot8581345348:AAH0HJxhs1hJonuQ3ZiQBYo5JLsCRVlKeiU`;
+const CHAT_ID = '-5011906253';
 
 export async function sendTelegramMessage(data: any): Promise<void> {
     try {
